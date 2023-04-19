6191dd65208be28e30b54006df32d06f4b38b7a6
“Sergey” <s22-01-15@yandex.ru>
2023-04-20 03:31:58 +1000

    Ручной ввод списков заменен на рандомный task22

--- task22/main.py
+++ task22/main.py
@@ -5,7 +5,19 @@
 # Пользователь вводит в строку первый список затем 
 # на следующией строке второй список.
 
-n = set (map(int, input('Введите первый список значений через пробел: ').split()))
-m = set (map(int, input('Введите второй список значений через пробел: ').split()))
+# Доп. материалы, которые мне помогли:
+# https://all-python.ru/osnovy/mnozhestva.html
+# https://proproprogs.ru/python_base/sortirovka-sort-i-sorted
 
-print (*n.intersection(m))
+# n = set (map(int, input('Введите первый список значений через пробел: ').split()))
+# m = set (map(int, input('Введите второй список значений через пробел: ').split()))
+
+from random import randint
+
+len_n = int(input('Введите длину первого списка: '))
+n = set(randint(1, 100) for i in range(len_n))
+# print(n)
+len_m = int(input('Введите длину второго списка: '))
+m = set(randint(1, 100) for i in range(len_m))
+# print(m)
+print (*sorted(n.intersection(m)))
